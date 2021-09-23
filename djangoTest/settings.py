8f69d36324520a744d49e2629432a720e55b7097
DuncanCos <60133572+DuncanCos@users.noreply.github.com>
2021-09-23 19:07:25 +0200

    Add files via upload

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -134,5 +134,3 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-os.path.join(BASE_DIR, '/static/json/credentials.json')
