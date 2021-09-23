b2646c4b3d873dc485aeb0a492089ce283dc1272
DuncanCos <60133572+DuncanCos@users.noreply.github.com>
2021-09-23 21:10:01 +0200

    Delete addAnniv directory

--- addAnniv/form.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length=100)
-    birthday = forms.CharField(label='date of birthday', max_length=10)
