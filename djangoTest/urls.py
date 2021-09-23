81aa9da99e42b568d7914cc9561f4de0463caa07
DuncanCos <60133572+DuncanCos@users.noreply.github.com>
2021-09-23 19:23:04 +0200

    Update urls.py

--- djangoTest/urls.py
+++ djangoTest/urls.py
@@ -25,7 +25,5 @@ urlpatterns = [
     path('showList/', include('listAnniv.urls')),
     path('new/', include('addAnniv.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
