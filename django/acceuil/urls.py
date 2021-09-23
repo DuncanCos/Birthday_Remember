1cac38acd84af848e7d626ae636f86b3f2eecb9f
DuncanCos <60133572+DuncanCos@users.noreply.github.com>
2021-09-23 17:31:34 +0200

    reset migration

--- django/acceuil/urls.py
+++ django/acceuil/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/<int:num>/', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
+ 
--- django/acceuil/views.py
+++ django/acceuil/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from addAnniv.models import donnee
+from datetime import date
 import datetime
 
 # Create your views here.
-def index (request, num):
+def index (request):
 
     final=0
     mot = "nothing"
-    print(datetime.date.today())
-    first=0
-    last=0
-
+    first = 0
+    last = 0
+    soon = 1000
     if donnee.objects.exists():
         first = donnee.objects.first().id
         last = donnee.objects.last().id
@@ -19,35 +19,61 @@ def index (request, num):
     d = str(datetime.date.today())
     day = d.split("-")
 
-    print(day[0])
-
     month=day[1]
     Day=day[2]
 
     po = 0
     i = first
     while i!=last+1:
-        print(i)
+        #print(f"is {i} to {last + 1}")
         if donnee.objects.filter(pk=i).exists():
             yes=donnee.objects.get(pk=i)
             birthdate = yes.birthday
             birthDate = birthdate.split('/')
             dayDate = birthDate[0]
             monthDate = birthDate[1]
-            final+=1
-            if monthDate == month and dayDate == Day:
-                po=i
-            i+=1
-        else:
-            i+=1
 
-    if po == 0:
-        motus = "nobody"
+            d1 = date(2021,int(monthDate),int(dayDate))
+            d2 = date.today()
+
+
+            """
+            fixer le probleme de soustraction
+
+            la soustraction est tout le temp positif 
+            trouver un moyen de faire en sorte de differencier les 
+            anniversaire positif et negatif
+            """
+
+            result1 = abs(d1-d2).days
+            print(abs(d2-d1).days)
+            print (datetime.datetime.today().day)
+            dt = datetime.datetime.today().day
+            if result1 == 0:
+                po = i
+                soon = result1
+                i += 1
+            elif result1 > 0 and int(dayDate) > dt and result1 < soon:
+                po = i
+                soon = result1
+                i += 1
+            else:
+                i += 1
+        else:
+            i += 1
+
+    print(f"{po} and {soon}")
+
+    if po == 0 and soon == 1000:
+        mot= "no birthday today"
+    elif po !=0 and soon > 0:
+        motus= donnee.objects.get(pk=po).name
+        
+        mot = (f"it is the birthday of {motus} in {soon} days")
     else:
         motus= donnee.objects.get(pk=po).name
-    mot="THIS IS THE BIRTHDAY OF {} TODAY".format(motus)
-
-
-
-
+        mot=(f"THIS IS THE BIRTHDAY OF {motus} TODAY")
+    
+        
     return render (request, 'acceuil.html',{'mot': mot})
+ 
