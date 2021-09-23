2cc4cc57320fa7ce0d71b5137d2c68c6821ae073
DuncanCos <60133572+DuncanCos@users.noreply.github.com>
2021-09-23 18:41:24 +0200

    Add files via upload

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(a^=ogp)=$%07)n*(clstfzyi#05rdy*1p5$(m*lzba0$(-!!v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
